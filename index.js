fedaf788d7aa00005c020f67e86e273ab9d26d32
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2023-08-02 14:41:55 -0300

    Conectando front com back

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
+io.on("connection", (socket) => {
+  console.log(socket);
+  console.log(socket.id)
+});
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
