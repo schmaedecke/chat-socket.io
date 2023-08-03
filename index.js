5f95e0c0ea2559c6a225e91b991a2d9451a4e2d5
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2023-08-03 16:22:27 -0300

    Finalizando procedimentos

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
 io.on("connection", (socket) => {
-  console.log(socket);
-  console.log(socket.id)
+  socket.on("boasvindas", (data) => {
+    console.log(data);
+  });
+
+  socket.on("palavra", data =>{
+    console.log(data)
+    socket.emit("resultado", data + " - teste")
+  })
+
 });
 
 app.set("view engine", "ejs");
